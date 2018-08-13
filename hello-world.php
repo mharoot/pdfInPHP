bef9feb9d206b3ff9b3120d24c58c536ea8e0920
yxw165730 <yxw165730@utdallas.edu>
2018-08-13 15:34:35 -0700

    images added for writing on top of with forms

--- hello-world.php
+++ hello-world.php
@@ -5,9 +5,26 @@
 // Minimal Example
 
 require_once 'fpdf/fpdf.php';
+/**
+ * After including the library file, we create an FPDF object. The constructor is used here with the
+ * default values: pages are in A4 portrait and the unit of measure is millimeter. It could have 
+ * been specified explicitly with:
+ *                                  $pdf = new FPDF('P','mm','A4');
+ */
 $pdf = new FPDF();
-$pdf->AddPage();
-$pdf->SetFont('Arial','B',16);
+$pdf->AddPage(); // There's no page at the moment, so we have to add one with AddPage(). 
+                 // The origin is at the upper-left corner and the current position is by default 
+                 // set at 1 cm from the borders; the margins can be changed with SetMargins(). 
+
+
+$pdf->SetFont('Arial','B',16); // Before we can print text, it's mandatory to select a font with SetFont(). We choose Arial bold 16:
+    
 $pdf->Cell(40,10,'Hello World!');
+$pdf->Cell(40,10,'Hello World2!');
+$pdf->Cell(40,10,'Hello World3!');
+// Insert a logo in the top-left corner at 300 dpi
+// $pdf->Image('logo.png',10,10,-300);
+// Insert a dynamic image from a URL
+$pdf->Image('http://chart.googleapis.com/chart?cht=p3&chd=t:60,40&chs=250x100&chl=Hello|World',60,30,90,0,'PNG');
 $pdf->Output();
 ?>
