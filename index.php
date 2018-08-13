c9a11d641a7b35313b7b75735faf0d19e9f0f7c7
yxw165730 <yxw165730@utdallas.edu>
2018-08-13 15:02:26 -0700

    pdf library found

--- index.php
+++ index.php
@@ -1,34 +1,14 @@
 <?php
-$p = PDF_new();
+// http://www.fpdf.org/en/tutorial/tuto1.htm
+// http://www.fpdf.org/en/doc/index.php
+require_once 'fpdf/fpdf.php';
 
-/*  open new PDF file; insert a file name to create the PDF on disk */
-if (PDF_begin_document($p, "", "") == 0) {
-    die("Error: " . PDF_get_errmsg($p));
-}
+// Minimal Example
+require('fpdf.php');
 
-PDF_set_info($p, "Creator", "hello.php");
-PDF_set_info($p, "Author", "Rainer Schaaf");
-PDF_set_info($p, "Title", "Hello world (PHP)!");
-
-PDF_begin_page_ext($p, 595, 842, "");
-
-$font = PDF_load_font($p, "Helvetica-Bold", "winansi", "");
-
-PDF_setfont($p, $font, 24.0);
-PDF_set_text_pos($p, 50, 700);
-PDF_show($p, "Hello world!");
-PDF_continue_text($p, "(says PHP)");
-PDF_end_page_ext($p, "");
-
-PDF_end_document($p, "");
-
-$buf = PDF_get_buffer($p);
-$len = strlen($buf);
-
-header("Content-type: application/pdf");
-header("Content-Length: $len");
-header("Content-Disposition: inline; filename=hello.pdf");
-print $buf;
-
-PDF_delete($p);
+$pdf = new FPDF();
+$pdf->AddPage();
+$pdf->SetFont('Arial','B',16);
+$pdf->Cell(40,10,'Hello World!');
+$pdf->Output();
 ?>
